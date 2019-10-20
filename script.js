b0342dfe506dbfd113ffbd83075e627e3b08d578
AnatolijDm <anat_dr25@rambler.ru>
2019-10-21 02:02:23 +0300

    Feat: modify_js

--- script.js
+++ script.js
@@ -46,3 +46,66 @@ document.querySelector('.control.right').addEventListener('click', function() {
         nextItem(currentItem);
     };
 })
+
+const swipedetect = (el) => {
+    let surface = el;
+    let startX = 0;
+    let startY = 0;
+    let distX = 0;
+    let distY = 0;
+    
+    let startTime = 0;
+    let elapsedTime = 0;
+    
+    let threshold = 150;
+    let restraint = 100;
+    let allowedTime = 300;
+
+    surface.addEventListener('touchstart', function(e) {
+        if (e.target.classList.contains('arrow') || e.target.classList.contains('control')) {
+            if (e.target.classList.contains('left')) {
+                if (isEnabled) {
+                    previousItem(currentItem);
+                }
+            } else if (e.target.classList.contains('right')) {
+                if (isEnabled) {
+                    nextItem(currentItem);
+                }
+            }
+        } 
+        let touchObj = e.changeTouches[0];
+        startX = touchObj.pageX;
+        startY = touchObj.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+    });
+    
+    surface.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+    });
+
+    surface.addEventListener('touchend', function(e) {
+        let touchObj = e.changeTouches[0];
+        distX = touchObj.pageX - startX;
+        distY = touchObj.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+       
+        if (elapsedTime <= allowedTime) {
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                if (distX > 0) {
+                    if (isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else {
+                    if (isEnabled) {
+                        nextItem(currentItem);
+                    } 
+                }
+            }
+        }
+        e.preventDefault();
+    });
+};
+
+let el = document.querySelector('.slider');
+swipedetect(el);
